f45ce91d7d472e1c96691d1f512c200535f04bb0
Tarcisio20 <tarcisior19@hotmail.com>
2022-05-18 17:57:34 -0300

    Subindo arquivos de js e css

--- src/views/partials/footer.php
+++ src/views/partials/footer.php
@@ -1,5 +1,6 @@
     <footer>
         Todos os direitos reservados!
     </footer>
+    <script type="text/javascript" src="<?php echo $base; ?>/assets/js/javascript.js"></script>
     </body>
 </html>
--- src/views/partials/header.php
+++ src/views/partials/header.php
@@ -5,6 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Document</title>
+        <link rel="stylesheet" type="text/css" href="<?php echo $base; ?>/assets/css/styles.css" media="screen" />
     </head>
     <body>
         <div>
@@ -13,6 +14,8 @@
             <li><a href="<?php echo $base; ?>/shipping">TRANSPORTADORAS</a></li>
             <li><a href="<?php echo $base; ?>/contestation">CONTESTAÇÕES</a></li>
             <li><a href="<?php echo $base; ?>/supplie">ABASTECIMENTOS</a></li>
+            <li><a href="<?php echo $base; ?>/batch">LOTE</a></li>
+            <li><a href="<?php echo $base; ?>/request">PEDIDOS</a></li>
             <li><a href="<?php echo $base; ?>/atm">ATMS</a></li>
         </ul>
     </div>
